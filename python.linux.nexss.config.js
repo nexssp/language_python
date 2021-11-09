43089066b86fbc2d78940a3fc26757b7c720c43e
Marcin Polak <mapoart@gmail.com>
2021-11-10 00:01:49 +0100

    Match the same commands for linux

--- python.linux.nexss.config.js
+++ python.linux.nexss.config.js
@@ -55,23 +55,24 @@ switch (distName) {
     break;
 }
 
-languageConfig.languagePackageManagers = {
-  pip3: {
-    installation: "choco install pip3",
-    messageAfterInstallation: "", //this message will be displayed after this package manager installation, maybe some action needed etc.
-    req: "python3 -m pip install -r requirements.txt",
-    freeze: "python3 -m pip freeze > requirements.txt",
-    installed: "python3 -m pip list",
-    search: "python3 -m pip search",
-    install: "python3 -m pip install",
-    uninstall: "python3 -m pip remove",
-    help: "python3 -m pip",
-    version: "python3 -m pip --version",
-    init: () => {},
-    // if command not found in specification
-    // run directly on package manager
-    else: "pip3",
-  },
-};
+// for Linux are the same commands
+// languageConfig.languagePackageManagers = {
+//   pip3: {
+//     installation: "choco install pip3",
+//     messageAfterInstallation: "", //this message will be displayed after this package manager installation, maybe some action needed etc.
+//     req: "python3 -m pip install -r requirements.txt",
+//     freeze: "python3 -m pip freeze > requirements.txt",
+//     installed: "python3 -m pip list",
+//     search: "python3 -m pip search",
+//     install: "python3 -m pip install",
+//     uninstall: "python3 -m pip remove",
+//     help: "python3 -m pip",
+//     version: "python3 -m pip --version",
+//     init: () => {},
+//     // if command not found in specification
+//     // run directly on package manager
+//     else: "pip3",
+//   },
+// };
 
 module.exports = languageConfig;
